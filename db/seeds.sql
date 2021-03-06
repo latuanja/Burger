0a4671831697e9c36d992b9e163e703e3419a344
Music Box <latuanja.c@gmail.com>
2021-03-06 13:39:46 -0600

    added express requirement

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Still in the `db` folder, create a `seeds.sql` file. In this file, write insert queries to populate the `burgers` table with at least three entries.
+INSERT INTO burgers (burger_name) VALUES ('Beyond Burger', false);
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger', false);
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger', false);
