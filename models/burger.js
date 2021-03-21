d5e0a4a8c3957980baa07ce6d1449ee19fefb53e
Music Box <latuanja.c@gmail.com>
2021-03-21 18:02:18 -0500

    Added event listeners and buttons to burger.js, added views and layouts for handlebar files(index & main), and added css

--- models/burger.js
+++ models/burger.js
@@ -14,5 +14,79 @@ const burger = {
     },
 
 };
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    if (event) {
+      console.info("DOM loaded");
+    }
+ 
+    const changeBurgerBtns = document.querySelectorAll(".devoured");
+  
+
+    if (changeBurgerBtns) {
+      changeBurgerBtns.forEach((button) => {
+        button.addEventListener("click", (e) => {
+          e.preventDefault();
+
+          const id = e.target.getAttribute("data-id");
+          const devouredBurg = e.target.getAttribute("data-devouredBurg");
+  
+          const newBurgerState = {
+            devoured: true,
+          };
+  
+          fetch(`/api/burgers/${id}`, {
+            method: "PUT",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+  
+      
+            body: JSON.stringify(newBurgerState),
+          }).then((response) => {
+
+            if (response.ok) {
+              console.log(`changed devour to: ${devouredBurg}`);
+              location.reload("/");
+            } else {
+              alert("something went wrong!");
+            }
+          });
+        });
+      });
+    }
+  
+    // CREATE
+    const createBurgerBtn = document.getElementById("create-btn");
+  
+    if (createBurgerBtn) {
+      createBurgerBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+  
+        const newBurger = {
+          burger_name: document.getElementById("burgerName").value.trim(),
+          devoured: false,
+        };
+        console.log(newBurger);
+  
+        fetch("/api/burgers", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+  
+          body: JSON.stringify(newBurger),
+        }).then(() => {
+        
+          document.getElementById("burgerName").value = "";
+  
+          console.log("Created a new burger!");
+          location.reload();
+        });
+      });
+    }
+  });
 // Export at the end of the `burger.js` file
 module.exports = burger;
